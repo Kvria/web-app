65c6803dc99ee130203dc2c5042101e5c6fc1e89
Kvria <trishakuria@gmail.com>
2020-02-15 15:38:28 +0300

    add variables to js script

---
+++ js/scripts.js
@@ -0,0 +1,3 @@
+var maleNames={"Kwasi","Kwandwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"}
+var femaleNames={"Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"}
+var dayOfTheWeek={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"}

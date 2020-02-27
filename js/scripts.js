3b0e4b13a0c1af71d76bfb8a2ba86900d03e7d0b
Kvria <trishakuria@gmail.com>
2020-02-27 10:40:04 +0300

    add new fomula

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,4 @@
-//var check;
-//var check2;
+
 var gender;
 var CC;
 var YY;
@@ -12,13 +11,10 @@ function submitBirthday(e) {
   e.preventDefault();
   month = parseInt(document.getElementById("month").value);
   date = parseInt(document.getElementById("date").value);
-  //check = document.getElementById("Check").value;
-  //check2=document.getElementById("Check2").value;
+  
   gender=document.getElementById("gender").value;
 
-  CC=parseInt(document.getElementById("year").value.slice(0,2))
-  YY=parseInt(document.getElementById("year").value.slice(2,4))
-
+  year=parseInt(document.getElementById("year").value);
   if ( month <= 0 || month > 12) {
     alert("Key in valid month.");
     return;
@@ -27,49 +23,30 @@ function submitBirthday(e) {
     alert("Key in valid date.");
     return;
   }
-  if ( year = 0 || year > 2020) {
+  if ( year === 0 || year > 2020) {
     alert("Key in valid year.")
     return;
   }
   findAkanName();
 }
 function findAkanName (){
-  var dateweek = Math.floor( ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*( month +1)/10)) + date ) % 7);
+  const day = new Date(`${date}-${month}-${year}`)
+  let dateweek = day.getDay()
+  console.log(`${date}-${month}-${year}`)
 
   var days = ["Sunday","Monday","Tuesday","Wednsesday","Thursday","Friday","Saturday"];
   var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
   var female = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-  /*if ( check.checked == true) {
+
+  if(gender === "male"){
     alert("You are a male who was born on a " +days[dateweek] + ".Your Akan name is, "+male[dateweek]);
     return;
-
-
-  }
-  if (check2.checked == true) {
-    alert("You are a female who was born on a " +days[dateweek] + ".Your Akan name is, "+female[dateweek]);
-    return;
-
-
-  }*/
-  if(gender==="male"){
-    alert("You are a male who was born on a " +days[dateweek] + ".Your Akan name is, "+male[dateweek]);
-    return;
-    
-
   }
   else if(gender==="female"){
     alert("You are a female who was born on a " +days[dateweek] + ".Your Akan name is, "+female[dateweek]);
     return;
-    
-
   }
-
-  
-
-
-
-    
-  else {
+else {
     alert("Key in the correct data.");
   }
 }
